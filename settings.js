97c1dc08f409461d6f9fc489c3d7434a1adc2b29
tasneembabanta-droid <tasneembabanta@gmail.com>
2026-03-30 05:46:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Tasta', // Your name
-  ownerNumber: '2349032296111', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348033891568', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
